9f3ce0eb723da60fecca8b6d1b9eb6ba3940ab87
Thomas Staud <thomas.staud05@gmail.com>
2025-07-28 15:53:13 +0200

    package upgrades

--- client/src/api.js
+++ client/src/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export async function get_weather() {
-    const response = await axios.get(`http://10.0.0.7:5000/weather`, {});
+    const response = await axios.get(`http://192.168.178.100:5000/weather`, {});
     return response.data;
 }
 
 export async function get_todos_today() {
-    const response = await axios.get(`http://10.0.0.7:5000/todos/today`, {});
+    const response = await axios.get(`http://192.168.178.100:5000/todos/today`, {});
     return response.data;
 }
 
 export async function save_todo(text, date) {
-    const response = await axios.post(`http://10.0.0.7:5000/todos`, {text: text, date: date});
+    const response = await axios.post(`http://192.168.178.100:5000/todos`, {text: text, date: date});
     return response.data;
 }
